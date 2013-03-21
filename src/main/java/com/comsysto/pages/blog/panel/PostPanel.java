7b89c3a9eeb97a5719f229c61fc4c8d64b6dc044
Sekib Omazic <sekib.omazic@comsysto.com>
2013-03-21 14:02:57 +0100

    comment list added

--- src/main/java/com/comsysto/pages/blog/panel/PostPanel.java
+++ src/main/java/com/comsysto/pages/blog/panel/PostPanel.java
@@ -1,14 +1,19 @@
 package com.comsysto.pages.blog.panel;
 
+import com.comsysto.domain.Comment;
 import com.comsysto.domain.Post;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 
 import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -28,21 +33,28 @@ public class PostPanel extends Panel {
         content(model);
 
         commentForm(model);
+
+        comments(model);
     }
 
     private void title(IModel<Post> model) {
         add(new Label("title", new PropertyModel<String>(model, "title")));
     }
 
+    private String postedBy(String user, Date date) {
+        Locale locale = getLocale();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.MEDIUM, locale);
+        String by = (locale == Locale.UK) ? "by" : "von";
+        return dateFormat.format(date) + " " + by + " " + user;
+    }
+
     private void dateAndUser(final IModel<Post> model) {
         add(new Label("date_user", new AbstractReadOnlyModel<String>() {
             @Override
             public String getObject() {
-                Post post = model.getObject();
-                Locale locale = getLocale();
-                DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, locale);
-                String by = (locale == Locale.UK) ? "by" : "von";
-                return dateFormat.format(post.getUpdated()) + " " + by + " " + post.getUser().getFullName();
+                String user = model.getObject().getUser().getFullName();
+
+                return postedBy(user != null ? user : "Anonymous", model.getObject().getUpdated());
             }
         }));
     }
@@ -59,5 +71,27 @@ public class PostPanel extends Panel {
         add(new CommentFormPanel("commentForm", model));
     }
 
+    private void comments(IModel<Post> model) {
+
+        IModel<List<Comment>> commentListModel = new PropertyModel<List<Comment>>(model, "comments");
+
+        ListView<Comment> commentList = new ListView<Comment>("comments", commentListModel) {
+            @Override
+            protected void populateItem(final ListItem<Comment> item) {
+                item.add(new Label("name_time", new AbstractReadOnlyModel<String>() {
+                    @Override
+                    public String getObject() {
+                        String user = item.getModelObject().getName();
+
+                        return postedBy(user != null ? user : "Anonymous", item.getModelObject().getCreated());
+                    }
+                }));
+
+                item.add(new MultiLineLabel("content", new PropertyModel<String>(item.getModel(), "content")));
+            }
+        };
+
+        add(commentList);
+    }
 
 }
